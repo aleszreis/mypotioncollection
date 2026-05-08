3eee69f3dc111ea0fa95e365dfdce4337117dc6f
Alessandra <alereis95@gmail.com>
2026-05-08 13:41:13 -0300

    :bug: disable send fetch button if fuel not enough

--- FetchItemUI.gd
+++ FetchItemUI.gd
@@ -1,11 +1,15 @@
 extends Control
 
+@onready var fetch_enabled: Panel = $FetchEnabled
+@onready var fetch_in_progress: Panel = $FetchInProgress
+
 @onready var inventory_ui: Control = $InventoryUI
-@onready var items_list: OptionButton = $Panel/VBoxContainer/ChooseItem
-@onready var cats_list: OptionButton = $Panel/VBoxContainer/ChooseCat
-@onready var progress_bar = $Panel/VBoxContainer/ProgressBar
-@onready var progress_label: Label = $Panel/VBoxContainer/ProgressLabel
-@onready var selected_grid: GridContainer = $Panel/VBoxContainer/ItemsToConsume
+@onready var items_list: OptionButton = $FetchEnabled/VBoxContainer/ChooseItem
+@onready var cats_list: OptionButton = $FetchEnabled/VBoxContainer/ChooseCat
+@onready var progress_bar = $FetchEnabled/VBoxContainer/ProgressBar
+@onready var progress_label: Label = $FetchEnabled/VBoxContainer/ProgressLabel
+@onready var selected_grid: GridContainer = $FetchEnabled/VBoxContainer/ItemsToConsume
+@onready var send_button: Button = $FetchEnabled/VBoxContainer/MarginContainerBtn/HBoxContainer/SendButton
 
 var all_cats_data = []
 var selected_cat : CatInstance
@@ -17,7 +21,7 @@ var selector : SelectionController
 
 func _ready():
 	selector = inventory_ui.selector
-	selector.MAX_SELECTION = 28
+	selector.MAX_SELECTION = 16
 	selector.selected_item.connect(_on_fuel_selected)
 	selector.deselected_item.connect(_on_fuel_deselected)
 	
@@ -68,11 +72,6 @@ func _clear_cats() -> void:
 	cats_list.clear()
 
 func _on_send_button_pressed():
-	if progress_bar.value < progress_bar.max_value:
-		# TODO: add friendly message/block button
-		print("Not enough fuel to send cat")
-		return
-		
 	var cat: CatInstance = cats_list.get_item_metadata(cats_list.get_selected_id())
 	var item: IngredientData = items_list.get_item_metadata(items_list.get_selected_id())
 	var now := Time.get_unix_time_from_system() + 86400 # Adds one day
@@ -81,11 +80,13 @@ func _on_send_button_pressed():
 	
 	print("FetchItemUI.gd: Sending cat %s to fetch item %s" % [cat.cat_data.display_name, item.display_name])
 	
-	# TODO: consume selected items
 	selector.clear()
+	close()
 	# TODO: verify that cat is gone, busy and with correct chosen_item
 	
-	close()
+	# TODO: disallow new fetch send - wait until cat comes back
+	#fetch_enabled.hide()
+	#fetch_in_progress.show()
 
 func _on_cancel_button_pressed():
 	close()
@@ -101,6 +102,7 @@ func _update_progress() -> void:
 	var fuel_value = _calculate_fuel_value()
 	progress_bar.value = fuel_value
 	progress_label.text = "%s / %s" % [fuel_value, int(progress_bar.max_value)]
+	send_button.disabled = fuel_value < int(progress_bar.max_value)
 
 func _on_fuel_selected(item_id: String) -> void:
 	# Cria slot
@@ -117,8 +119,6 @@ func _calculate_fuel_value() -> int:
 	
 	for item_id: String in fuel:
 		var item_rarity = IngDatabase.get_by_id(item_id).rarity
-		# TODO: update IDs
-		# talvez criar tudo como resource pra referenciar um ao outro?
 		if item_id == selected_cat.cat_data.favorite_item_id:
 			value += (item_rarity * 3)
 		else:
@@ -127,4 +127,3 @@ func _calculate_fuel_value() -> int:
 
 func _on_fuel_deselected() -> void:
 	_update_progress()
-	
